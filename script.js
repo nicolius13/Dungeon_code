dc17fb95857cce971e2485b2b03491330106caa3
Nicolas Vastrade <Nicolas.vastrade@gmail.com>
2020-03-19 09:37:58 +0700

    refactor  some code dup

--- script.js
+++ script.js
@@ -7,20 +7,33 @@ let map = [];
 
 // WEAPONS
 // to add weapon : key must be the same as the 'name' value and the name of the '.png'
-const weapons = {
-  sword: { name: 'sword', position: [], img: './Assets/img/Weapons/sword.png' },
-  hammer: { name: 'hammer', position: [], img: './Assets/img/Weapons/hammer.png' },
-  axe: { name: 'axe', position: [], img: './Assets/img/Weapons/axe.png' },
-  spear: { name: 'spear', position: [], img: './Assets/img/Weapons/spear.png' },
-};
-
+const weapons = [
+  { name: 'sword', position: [], img: './Assets/img/Weapons/sword.png' },
+  { name: 'hammer', position: [], img: './Assets/img/Weapons/hammer.png' },
+  { name: 'axe', position: [], img: './Assets/img/Weapons/axe.png' },
+  { name: 'spear', position: [], img: './Assets/img/Weapons/spear.png' },
+];
 // PLAYERS
-let player1 = { name: 'one', life: 100, damage: 10, position: [], img: './Assets/img/Players/knight_idle-sprit.png' };
-let player2 = {
-  name: 'two',
+let player1 = {
+  name: 'player1',
   life: 100,
-  damage: 10,
+  weapon: {
+    name: 'fist',
+    damage: 10,
+  },
   position: [],
+  turn: true,
+  img: './Assets/img/Players/knight_idle-sprit.png',
+};
+let player2 = {
+  name: 'player2',
+  life: 100,
+  weapon: {
+    name: 'fist',
+    damage: 10,
+  },
+  position: [],
+  turn: false,
   img: './Assets/img/Players/mage_idle-sprit.png',
 };
 
@@ -55,22 +68,23 @@ function chooseCoordObj(x, y) {
   return [coordX, coordY];
 }
 // place an object in the map array
-function placeObj(type, arg) {
+function putObjInMap(type, weapon) {
+  let objPlaced;
+
   if (type === 'weapons') {
-    map[weapons[arg].position[0]][weapons[arg].position[1]].obj = arg;
+    objPlaced = weapon;
   } else {
-    let player;
     if (type === 'player1') {
-      player = player1;
+      objPlaced = player1;
     } else if (type === 'player2') {
-      player = player2;
+      objPlaced = player2;
     }
-    map[player.position[0]][player.position[1]].obj = type;
   }
+  map[objPlaced.position[0]][objPlaced.position[1]].obj = objPlaced.name;
 }
-function placePlayers(obj, className, x, y) {
+function placeObj(obj, className, x, y) {
   obj.position = chooseCoordObj(x, y);
-  placeObj(className);
+  putObjInMap(className, obj);
 }
 
 // randomly choose style of the floors tiles
@@ -146,19 +160,21 @@ function generateMap(x, y) {
   }
   // WEAPONS
   // create an array of the keys of the weapons obj to loop through
-  const weapVal = Object.keys(weapons);
-  weapVal.forEach(weap => {
+  weapons.forEach(weapon => {
     // choose position of weapon and put it in the weapons obj
-    weapons[weap].position = chooseCoordObj(x, y);
-    placeObj('weapons', weap);
+    placeObj(weapon, 'weapons', x, y);
   });
   // PLAYERS
-  placePlayers(player1, 'player1', x, y);
-  placePlayers(player2, 'player2', x, y);
+  placeObj(player1, 'player1', x, y);
+  placeObj(player2, 'player2', x, y);
 
   // render the map
   renderTile();
 }
+
+//  /////////////////////////
+//         MOVEMENT
+//  /////////////////////////
 
 //  /////////////////////////
 //         GAME START
