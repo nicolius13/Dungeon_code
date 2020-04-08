8dc8496c0820157d18d30486f42884480342e22c
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-04-08 16:14:38 +0700

    delete deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Dungeon_deploy/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Dungeon_code/' : '/',
 };
